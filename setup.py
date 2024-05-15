fe410d8abf22ca84beb70565341866bd138bf0d6
Tushar Saini <tusharsaini1854@gmail.com>
2024-05-15 19:35:32 +0530

    [UPD] update version

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
-def read( fname ):
+
+def read(fname):
     with open(fname) as fp:
         content = fp.read()
     return content
-    
+
+
 setup(
     name="retailtree",
-    version="1.1",
+    version="1.2",
     long_description=read("README.md"),
     long_description_content_type='text/markdown',
     packages=find_packages(),
