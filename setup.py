d80cb5de47ee41703275099b6b4d9b182e5b8bbb
Ahwan Kumar <ahwan.monu@gmail.com>
2024-05-15 16:51:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="retailtree",
     version="0.0.1",
+    long_description=long_description=read("README.md")
     packages=find_packages(),
     install_requires=[
         'numpy',
