c43a4d697c4b634cf2d1a5a18083a7261f67ba90
Ahwan Kumar <ahwan.monu@gmail.com>
2024-05-15 16:54:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+def read( fname ):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+    
 setup(
     name="retailtree",
     version="0.0.1",
